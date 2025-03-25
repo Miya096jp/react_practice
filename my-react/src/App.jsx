87b520cb3956e937a563ee8897de9b25c97c9300
Miya096jp <miyawaki.yske@gmail.com>
2025-03-25 17:02:50 +0900

    App.jsxにベタ打ちしてUIを作成

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
+import "./App.css";
+
+let nextId = 3;
+const initial_data = [
+  { id: 0, title: "松尾芭蕉", body: "古池や蛙飛び込む水の音" },
+  { id: 1, title: "小林一茶", body: "痩せガエル負けるな一茶ここにあり" },
+  { id: 2, title: "作者不詳", body: "富士山麓に鸚鵡鳴く" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState(initial_data);
+  const [draft, setDraft] = useState(""); 
+  const [body, setBody] = useState(initial_body);
+  const [selectedId, setSelectedId] = useState(0);
+  const [showAdd, setShowAdd] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
+  const selectedNote = notes.find((note) => note.id === selectedId);
+  const initial_body = selectedNote.body
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <h1>Note app</h1>
+      <ul>
+        {notes.map((note) => (
+          <li
+            key={note.id}
+            onClick={() => {
+              setSelectedId(note.id);
+				      setShowEdit(true);
+				      setShowAdd(false);
+            }}
+          >
+            {note.title}
+          </li>
+        ))}
+					{showAdd ? 
+				   (
+				   <>
+            <textarea
+				     placeholder={"enter"}
+				     onChange={(e) => {
+              setDraft(e.target.value)}}
+				    />
+				    <br/>
+				    <button 
+				     onClick={() => {
+               const lines = draft.split("\n")
+								const title = lines.shift();
+								const body = lines.join();
+				        setNotes([
+								  ...notes,
+								  {id: nextId++, title: title, body: body }
+								])
+								setDraft("");
+								setShowAdd(!showAdd)}}
+									 >
+									 新規作成
+									 </button>
+				    </>
+					 )
+				:
+				   (
+            <li
+				     onClick={() => {
+										 setShowAdd(true);
+										 setShowEdit(false);
+						 }}
+									 >+</li>
+					 )
+					}
+      </ul>
+      {showEdit &&
+       (
+       <>
+        <textarea 
+				  value={selectedNote.body}
+				  onChange={(e) => {
+				    setDraft(e.target.value);
+					}}
+        />
+							 <br/>
+        <button onClick={() => 
+
+				  setShowEdit(!showEdit)}>更新</button>
+        <button onClick={() => {
+          setNotes(notes.filter(note => note.id !== selectedId));
+          setShowEdit(!showEdit)}}>削除</button>
+				<p>{draft}</p>
+      </>
+      )
+     }
+  </>
+  );
 }
 
-export default App
+export default App;
